4aa022ef5515c62f74acf95da8be791dfddf0d22
wingtatlee <wingtatlee@Kevins-MacBook-Pro.local>
2016-01-20 14:27:34 +1300

    fix data

--- app/src/main/java/com/davidpark/wise/model/DataOperation.java
+++ app/src/main/java/com/davidpark/wise/model/DataOperation.java
@@ -36,13 +36,13 @@ public class DataOperation extends Activity {
 
         //userID, CourseID, Title, View, Vote, Content, Date
         questionData.addQuestion(new Question(1, 1, "What is ISCG5420?", 0, 0, "Can someone tell me?", "2016"));
-        questionData.addQuestion(new Question(1, 1, "How is ISCG5420?", 0, 0, "Is it good?", "2016"));
-        questionData.addQuestion(new Question(2, 1, "Who is enrolling ISCG6421?", 0, 0, "Is it good?", "2016"));
-        questionData.addQuestion(new Question(3, 2, "I love ISCG 6211", 0, 0, "I love it", "2016"));
-        questionData.addQuestion(new Question(3, 2, "What is ISCG5400?", 0, 0, "Can someone tell me?", "2016"));
-        questionData.addQuestion(new Question(3, 3, "How is ISCG5400?", 0, 0, "Is it good?", "2016"));
-        questionData.addQuestion(new Question(2, 1, "Who is enrolling ISCG6421?", 0, 0, "Is it good?", "2016"));
-        questionData.addQuestion(new Question(2, 2, "I love ISCG 6211", 0, 0, "I love it", "2016"));
+        questionData.addQuestion(new Question(1, 1, "How can I enrol ISCG 5420?", 0, 0, "I dont know how to do this, pls tell me.", "2016"));
+        questionData.addQuestion(new Question(2, 1, "Can I study ISCG 5420 part time?", 0, 0, "I would like to study this course part time.", "2016"));
+        questionData.addQuestion(new Question(3, 2, "Is ISCG 6420 a compulsory course?", 0, 0, "I want to know this.", "2016"));
+        questionData.addQuestion(new Question(3, 2, "How will class be taught?", 0, 0, "Can someone tell me?", "2016"));
+        questionData.addQuestion(new Question(3, 2, "What is ISCG 6420?", 0, 0, "Is it good?", "2016"));
+        questionData.addQuestion(new Question(2, 3, "Who is enrolling ISCG 7420?", 0, 0, "Is it good?", "2016"));
+        questionData.addQuestion(new Question(2, 3, "Is it similar to ISCG 7430?", 0, 0, "I am not sure about if I choose this course.", "2016"));
     }
 
     public void addCommentData(){
@@ -50,7 +50,7 @@ public class DataOperation extends Activity {
         //UserID, QuestionID, Content, Date
         commentData.addComment(new Comment(1, 1, "Very good", "2016"));
         commentData.addComment(new Comment(2, 2, "Very good", "2016"));
-        commentData.addComment(new Comment(1, 2, "Very good", "2016"));
+        commentData.addComment(new Comment(1, 2, "i do not know", "2016"));
         commentData.addComment(new Comment(3, 3, "Very good", "2016"));
         commentData.addComment(new Comment(1, 4, "Very good", "2016"));
         commentData.addComment(new Comment(2, 3, "Very good", "2016"));
